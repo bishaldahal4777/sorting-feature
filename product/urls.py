a3aae9e217f9e053558962886978f697f3de64de
Bishal Dahal <bishaldahal4777@gmail.com>
2025-10-27 20:42:15 +0545

    new views

--- product/urls.py
+++ product/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.product, name= 'product'),
-    path('json/', views.json_example, name= 'json')
+    path('json/', views.json_example, name= 'json'),
+    path('home/', views.home, name= 'home'),
 
 
 ]
--- product/views.py
+++ product/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Product
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
+
 def product(request):
     context = {'title': 'Home Page', 'username':'Bishal dahal', 'developer': 'Bishal Dev', 'skills':['python', 'git', 'sql']}
     return render(request, 'product/product.html', context)
@@ -8,3 +9,7 @@ def product(request):
 def json_example(request):
     data = {'status':'success', 'Framework':'Django'}
     return JsonResponse(data)
+
+
+def home(request):
+    return HttpResponse("hey this is home page")
