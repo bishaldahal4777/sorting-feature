710ea4f82ca7eea84ffe82d65740870c796d0ba2
Bishal Dahal <bishaldahal4777@gmail.com>
2025-10-27 07:22:19 +0545

    views

---
+++ product/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from .models import Product
+
+def product_list(request):
+    sort_by = request.GET.get("sort","name")
+    Product.objects.all().order_by(sort_by)
+    return render(request, 'product/product_list.html', {'sort_by': sort_by})
