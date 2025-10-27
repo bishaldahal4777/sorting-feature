ea2ee168f2c1313a1ae85d8351bef42c9aab4b65
Bishal Dahal <bishaldahal4777@gmail.com>
2025-10-27 20:18:03 +0545

    html

--- product/views.py
+++ product/views.py
@@ -5,3 +5,6 @@ def product_list(request):
     sort_by = request.GET.get("sort","name")
     Product.objects.all().order_by(sort_by)
     return render(request, 'product/product_list.html', {'sort_by': sort_by})
+
+def product(request):
+    return render(request, 'product/product.html')
