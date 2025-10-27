56661fd8c5c82eb93ed7484a3b31076bf88b42bf
Bishal Dahal <bishaldahal4777@gmail.com>
2025-10-27 20:22:32 +0545

    html and map

--- product/views.py
+++ product/views.py
@@ -1,10 +1,6 @@
 from django.shortcuts import render
 from .models import Product
 
-def product_list(request):
-    sort_by = request.GET.get("sort","name")
-    Product.objects.all().order_by(sort_by)
-    return render(request, 'product/product_list.html', {'sort_by': sort_by})
-
 def product(request):
-    return render(request, 'product/product.html')
+    context = {'title': 'Home Page', 'username':'Bishal dahal'}
+    return render(request, 'product/product.html', context)
