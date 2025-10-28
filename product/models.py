728d72c83da393a4e1882ff4adc0c826cb434697
Bishal Dahal <bishaldahal4777@gmail.com>
2025-10-28 07:45:12 +0545

    models

--- product/models.py
+++ product/models.py
@@ -9,3 +9,11 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Feedback(models.Model):
+    name = models.CharField(max_length=100)
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} - {self.message[:30]}"
